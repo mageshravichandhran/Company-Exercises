3e04ca2b1f3b336ce46d7d64feeeb9cb078de7f6
mageshravichandhran <58700838+mageshravichandhran@users.noreply.github.com>
2021-09-14 18:21:21 +0530

    Add files via upload

---
+++ Class14.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Set2
+{
+    class Counters
+    {
+        string str;
+        public Counters(string str)
+        {
+            this.str = str;
+        }
+
+        public void count()
+        {
+
+            for (int i = 0; i < this.str.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < this.str.Length; j++)
+                {
+                    if (str[i] == str[j])
+                    {
+
+                        count += 1;
+
+
+
+                    }
+
+
+
+
+                }
+
+
+
+                Console.WriteLine("The count of {0} is {1}", str[i], count);
+            }
+        }
+
+
+
+    }
+}
+
+
---
+++ Class28.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Set2
+{
+    class CurrentBill
+    {
+        int units;
+        static int stage1=2;
+        static int stage2=6;
+        static int stage3=8;
+
+      
+        public CurrentBill(int units)
+        {
+            this.units = units;
+        }
+
+        public int BillCalc()
+        {
+           
+
+            if (this.units < 100)
+            {
+               return 0;
+            }
+
+            else if(this.units>=100 && this.units <= 200)
+            {
+                return this.units * stage1;
+            }
+
+            else if(this.units>=200 && this.units <= 400)
+            {
+                return this.units * stage2;
+            }
+
+            else
+            {
+                return this.units * stage3;
+            }
+
+       
+
+        }
+
+    }
+    
+}
